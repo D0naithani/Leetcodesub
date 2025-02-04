2c1401b92bd23438267cd672b3f9c4c79b1c95e5
D0naithani <divanaithani7@gmail.com>
2025-02-04 23:54:44 +0530

    Time: 3 ms (93.64%) | Memory: 12.3 MB (66.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,19 +1,6 @@
 class Solution:
     def isPalindrome(self, x):
-        # Edge case: Negative numbers and numbers ending with 0 (except 0 itself)
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-        
-        reversed_half = 0
-        
-        # Reverse half of the digits
-        while x > reversed_half:
-            reversed_half = reversed_half * 10 + x % 10
-            x //= 10
-        
-        # Explicitly check both conditions
-        if x == reversed_half:
-            return True
-        if x == reversed_half // 10:
-            return True
-        return False
+        if x < 0 or (x % 10 == 0 and x != 0): return False
+        r = 0
+        while x > r: r, x = r * 10 + x % 10, x // 10
+        return x == r or x == r // 10
