b57de8bb456b13b636e636e85562955250278165
D0naithani <divanaithani7@gmail.com>
2025-02-05 00:08:35 +0530

    Time: 91 ms (78.82%) | Memory: 20.7 MB (46.49%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxArea(self, height):
-        left = 0
-        right = len(height) - 1
+        left, right = 0, len(height) - 1
         max_area = 0
         
         while left < right:
-            # Calculate the current area
-            current_area = min(height[left], height[right]) * (right - left)
-            # Update the maximum area
-            max_area = max(max_area, current_area)
+            # Calculate the height and width
+            h = min(height[left], height[right])
+            w = right - left
+            area = h * w
             
-            # Move the pointer pointing to the shorter line
+            # Update max_area
+            if area > max_area:
+                max_area = area
+            
+            # Move the pointer corresponding to the shorter line
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
         
         return max_area
-
-# Example usage:
-solution = Solution()
-height1 = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-print(solution.maxArea(height1))  # Output: 49
-
-height2 = [1, 1]
-print(solution.maxArea(height2))  # Output: 1
