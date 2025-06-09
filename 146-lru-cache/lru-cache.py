86722a18150ec2858f58dd1793fa8e61d8fd10c7
D0naithani <divanaithani7@gmail.com>
2025-06-09 23:11:36 +0530

    Time: 145 ms (50.52%) | Memory: 78.2 MB (89.94%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,52 +1,68 @@
-class Node:
-    def __init__(self, key, val):
+class ListNode:
+    def __init__(self, key: int, value: int):
         self.key = key
-        self.val = val
+        self.value = value
         self.prev = None
         self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}  # maps key to node
-
-        # Create dummy head and tail nodes
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.cache = {}
+        self.size = 0
+        
+        # Dummy head and tail
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
+        
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    # Remove node from list
-    def _remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    # Add node to front
-    def _add_to_front(self, node):
-        node.next = self.head.next
+    def _add(self, node: ListNode):
+        # Always add node right after head
         node.prev = self.head
+        node.next = self.head.next
         self.head.next.prev = node
         self.head.next = node
+
+    def _remove(self, node: ListNode):
+        # Remove node from its current position
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _move_to_head(self, node: ListNode):
+        self._remove(node)
+        self._add(node)
+
+    def _pop_tail(self):
+        # Remove the last node before tail
+        lru_node = self.tail.prev
+        self._remove(lru_node)
+        return lru_node
 
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
-            self._remove(node)
-            self._add_to_front(node)
-            return node.val
+            self._move_to_head(node)
+            return node.value
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._remove(self.cache[key])
-        node = Node(key, value)
-        self._add_to_front(node)
-        self.cache[key] = node
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            node = ListNode(key, value)
+            self.cache[key] = node
+            self._add(node)
+            self.size += 1
 
-        if len(self.cache) > self.capacity:
-            # Remove from tail
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
+            if self.size > self.capacity:
+                # Evict LRU (tail.prev)
+                lru_node = self.tail.prev
+                self._remove(lru_node)
+                del self.cache[lru_node.key]
+                self.size -= 1
